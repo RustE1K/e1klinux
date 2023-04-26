3483a95ee2700e637d0d02ca0bd7bd80e39df370
Jesse Wei <jesse@cs.unc.edu>
2023-04-26 14:21:50 -0400

    Create rust_e1000.d

---
+++ samples/rust/rust_e1000.d
@@ -0,0 +1,13 @@
+samples/rust/rust_e1000.o: samples/rust/rust_e1000.rs samples/rust/e1000_defs.rs /home/jesse/Documents/e1klinux/rust/libcore.rmeta /home/jesse/Documents/e1klinux/rust/libcompiler_builtins.rmeta /home/jesse/Documents/e1klinux/rust/libkernel.rmeta /home/jesse/Documents/e1klinux/rust/libbindings.rmeta /home/jesse/Documents/e1klinux/rust/libmacros.so /home/jesse/Documents/e1klinux/rust/liballoc.rmeta /home/jesse/Documents/e1klinux/rust/libbuild_error.rmeta
+
+samples/rust/rust_e1000.d: samples/rust/rust_e1000.rs samples/rust/e1000_defs.rs /home/jesse/Documents/e1klinux/rust/libcore.rmeta /home/jesse/Documents/e1klinux/rust/libcompiler_builtins.rmeta /home/jesse/Documents/e1klinux/rust/libkernel.rmeta /home/jesse/Documents/e1klinux/rust/libbindings.rmeta /home/jesse/Documents/e1klinux/rust/libmacros.so /home/jesse/Documents/e1klinux/rust/liballoc.rmeta /home/jesse/Documents/e1klinux/rust/libbuild_error.rmeta
+
+samples/rust/rust_e1000.rs:
+samples/rust/e1000_defs.rs:
+/home/jesse/Documents/e1klinux/rust/libcore.rmeta:
+/home/jesse/Documents/e1klinux/rust/libcompiler_builtins.rmeta:
+/home/jesse/Documents/e1klinux/rust/libkernel.rmeta:
+/home/jesse/Documents/e1klinux/rust/libbindings.rmeta:
+/home/jesse/Documents/e1klinux/rust/libmacros.so:
+/home/jesse/Documents/e1klinux/rust/liballoc.rmeta:
+/home/jesse/Documents/e1klinux/rust/libbuild_error.rmeta:
